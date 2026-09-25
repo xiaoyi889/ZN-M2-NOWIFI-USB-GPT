bc7a407b5b6341f68370d2a035286036829880bc
xiaoyi889 <xiaoyi889@qq.com>
2026-09-25 17:34:55 +0800

    fix: import WOL packages individually

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -87,7 +87,9 @@ UPDATE_PACKAGE "partexp" "sirpdboy/luci-app-partexp" "main"
 UPDATE_PACKAGE "qbittorrent" "sbwml/luci-app-qbittorrent" "master" "" "qt6base qt6tools rblibtorrent"
 #UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
-UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus luci-app-wolultra"
+# WOL 系列第三方包：逐个提取到 package/ 根目录，避免把整个仓库作为一个无效 package 源
+UPDATE_PACKAGE "luci-app-wolultra" "VIKINGYFY/packages" "main" "pkg"
+UPDATE_PACKAGE "luci-app-wolplus" "sundaqiang/openwrt-packages" "master" "pkg"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 #更新软件包版本
