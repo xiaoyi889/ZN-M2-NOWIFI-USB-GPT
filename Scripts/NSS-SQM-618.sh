e7e0b0a324e8668ccc43221cb2ea4beac239ec3a
xiaoyi889 <xiaoyi889@qq.com>
2026-09-26 14:30:37 +0800

    fix: correct NSS SQM sqm-scripts Makefile injection

--- Scripts/NSS-SQM-618.sh
+++ Scripts/NSS-SQM-618.sh
@@ -110,9 +110,6 @@ apply_nss_sqm_618() {
 				print "\t\$(INSTALL_DIR) \$(1)/usr/lib/sqm"
 				print "\t\$(INSTALL_DATA) \$(TOPDIR)/package/zn-m2-nss-sqm-assets/nss-zk.qos \$(1)/usr/lib/sqm/nss-zk.qos"
 				print "\t\$(INSTALL_DATA) \$(TOPDIR)/package/zn-m2-nss-sqm-assets/nss-zk.qos.help \$(1)/usr/lib/sqm/nss-zk.qos.help"
-				print
-				print
-				print
 				print "endef"
 				in_install=0
 				inserted=1
